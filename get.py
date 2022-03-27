4909a685fbfa26cba13dd88fe3a9ecda1d6003ab
Nedyalko <ryderbg@gmail.com>
2022-03-28 00:58:16 +0300

    testing parser

--- get.py
+++ get.py
@@ -1,14 +1,30 @@
 import requests
+from bs4 import BeautifulSoup
 
 base_url = 'https://ozone.bg'
 category = '/laptopi-i-computri/mrejovi/routers/?marka='
 brand = 'asus'
 url = base_url+category+brand
 
-res = requests.get(url)
+page = requests.get(url)
 
-print(res.text)[:200]
+soup = BeautifulSoup(page.content, 'html.parser')
 
+top_items = []
+products = soup.select('div.products-list')
+print(products)
 
+#WIP
+for product in products:
+    title = product.select('span.title')[0].text
+    price = product.select('span.price')[0].text
+    info = {
+        "title": title.strip(),
+        "price": price.strip()
+    }
+    # print(title)
 
+    top_items.append(info)
+
+print(top_items)
 
