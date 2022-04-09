d1184bd32929a1f651d07036db2fb5842c71c3f5
Nedyalko <ryderbg@gmail.com>
2022-04-10 02:41:26 +0300

    to_csv - fixed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.modules.csv.to_csv import to_csv
 from src.products.getters.ozone import *
 from src.products.price_trigger import *
-category = '/laptopi-i-computri/mrejovi/routers/?marka='
+category = '/laptopi-i-computri/mrejovi/routers/?limit=100&marka='
 brand = 'asus'
 
 products = get_ozone(category, brand)
--- src/modules/csv/to_csv.py
+++ src/modules/csv/to_csv.py
@@ -1,11 +1,7 @@
 import csv
 def to_csv(products):
-    csv_rowlist = []
-    for product in products:   
-        csv_rowlist.append(f"[{product}]")
-    print(csv_rowlist)
-    with open('products.csv', 'w', newline='') as file:
-            writer = csv.writer(file)
-            # writer.writerow(["SN", "Product", "Price"])
-            writer.writerows(csv_rowlist)
-            # writer.writerow([2, "Harry Potter", "Harry Potter"])
+    with open('price_list.csv', 'w', newline='') as file:
+        fieldnames = ['title', 'price']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(products)
