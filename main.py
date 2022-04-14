bf60abfc09c293a37b17af1a5877672056ac826b
Nedyalko Demirev <33329482+ryderbg@users.noreply.github.com>
2022-04-14 21:54:13 +0300

    updated to dekada

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from src.products.find_product import find_product
 from src.modules.csv.to_csv import to_csv
-from src.products.getters.ozone import *
+from src.products.getters.dekada import *
 from src.products.price_trigger import *
-category = '/laptopi-i-computri/mrejovi/routers/?limit=100&marka='
-brand = 'asus'
+category = '/мишки-c-178.html'
+# category = '/laptopi-i-computri/mrejovi/routers/?limit=100&marka='
+# brand = 'asus'
 
-products = get_ozone(category, brand)
+products = get_dekada(category)
+print(products)
 to_csv(products)
-find_product(products, 'AX')
+# find_product(products, 'AX')
 # print(price_trigger(products))
--- src/modules/csv/to_csv.py
+++ src/modules/csv/to_csv.py
@@ -3,7 +3,7 @@ import csv
 
 def to_csv(products):
     with open('price_list.csv', 'w', newline='') as file:
-        fieldnames = ['title', 'price']
+        fieldnames = ['title', 'price', 'availability']
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(products)
